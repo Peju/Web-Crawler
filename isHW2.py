5dfd6d912344dadd352e81f559564c20616c9c6d
Peter Jough <pjough@gmail.com>
2019-02-23 23:35:39 -0500

    5

--- isHW2.py
+++ isHW2.py
@@ -4,91 +4,65 @@ import queue
 from bs4 import BeautifulSoup
 
 seedUrl = "https://en.wikipedia.org/wiki/Orange_(fruit)"
+seedUrl2 = "https://en.wikipedia.org/wiki/Citrus"
 compileURL = []
 tenWords = ['fruit','species','health','location','farming','history','nutrition','biology','food']
+compileURL.append(seedUrl)
+compileURL.append(seedUrl2)
+goodUrls = []
+	
+def check(url):
 
-qe = queue.Queue()
+    try:
+            testReq  = requests.get(url)
+            print(testReq)
+            counterN = 0
+            global counter
+            global compileURL
+            global goodUrls
+
+        
+            found = False
+
+            data = testReq.text
+
+            soupTest = BeautifulSoup(data,"html.parser")
+
+            testVar = soupTest.find_all('div',{'class': "mw-parser-output"})[0]
+            testVar2 = str(testVar).split()
+            print (len(testVar2))
+
+            for link in soupTest.find_all('a'):
+                #print(link.get('href'))
+                compileURL.append(link.get('href'))
+
+            for x in testVar2:
+                #print (x)
+                for y in tenWords:
+                    if x == y:
+                        counterN +=1
+                    if counterN == 2:
+                        found = True
+                        goodUrls.append(url)
+                        counter +=1
+                        print("2 terms have been matched")
+                        f = open(str(counter)+'DataCode.txt','w',encoding="utf8")
+                        f.write(str(testVar))
+                        f.close
+                        break
+                        
+                if found == True:
+                    break
+    except:
+            return
     
-def mainCheck(url):
-    testReq  = requests.get(url)
-    if testReq == 404:
-        return
-    print(testReq)
-    counterN = 0
+counter = 0 # counter for number of good sites
 
-    found = False
+while counter < 501 and compileURL:
+	check(compileURL.pop(0))
 
-    data = testReq.text
-
-    soupTest = BeautifulSoup(data,"html.parser")
-
-    testVar = soupTest.find_all('div',{'class': "mw-parser-output"})[0]
-    testVar2 = str(testVar).split()
-    print (len(testVar2))
-
-    for link in soupTest.find_all('a'):
-        #print(link.get('href'))
-        compileURL.append(link.get('href'))
-
-    for x in testVar2:
-        #print (x)
-        for y in tenWords:
-            if x == y:
-                counterN +=1
-            if counterN == 2:
-                found = True
-                print("2 terms have been matched")
-                f = open('DataCode.txt','w',encoding="utf8")
-                f.write(str(testVar))
-                f.close
-                break
-                
-        if found == True:
-            break
-  
-mainCheck(seedUrl)
-
-while(True):
-    print(str(len(compileURL)) + " links returned")
-    f = open('listurl.txt','w',encoding="utf8")
-    f.write(str(compileURL))
-    f.close
-    break
+f = open('listurl.txt','w',encoding="utf8")
+f.write(str(goodUrls))
+f.close
 
 
-"""
-pageResponse = requests.get(seedUrl, timeout = 3)
-
-for i in range (0,20):
-    chunks = contents.find_all("p"[i].text)
-    textCon.append(chunks
-
-for url in seedUrls:
-    seedUrls.append(url)
-    visitedUrlList.append(url)
-while queue is not empty:
-    url = queue.pop()
-    pageContent = retrievePageContent(url)
-    if pageContent is None:
-        continue
-    termCounter = 0
-    pageMainText = extractMainText(pageContent)
-    for term in relatedTerms:
-        if match (term, pageMainText, CaseInsensitive):
-            termCounter = termCounter+1
-            if termCounter >= 2:
-                pageTitle = getTitle(pageContent)
-                save(pageTitle, pageContent)
-                savedUrlList.add(url)
-                pageCounter = pageCounter+1
-                print("page # termCounter: url")
-                break
-    if pageCounter >= 500:
-        break
-    outGoingUrls = extractOutGoingUrl(pageContent)
-    for outGoingUrl in outGoingUrls:
-        if domainCheck(outGoingUrl) and outGoingUrl not in visitedUrlList:
-            queue.add(outGoingUrl)
-            visitedUrlList.add(outGoingUrl)
-save(savedUrlList)
- """
